fb01830f0a86e059dd824cf20a0ab4d4a14657d5
Kyotii <113300524+Kyotii@users.noreply.github.com>
2025-05-10 02:03:04 +0800

    USER END UI NO BACK END

---
+++ HumanRepProj/Pages/UserHelp.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HumanRepProj.Pages
+{
+    public class UserHelpModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
